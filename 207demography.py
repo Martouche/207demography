c540852e9cbf2fa5b9eab64b4c8118a77f11d4ed
vantalon <martin.vantalon@epitech.eu>
2019-04-29 11:30:25 +0200

    bases

---
+++ 207demography.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import sys, os, math, csv
+from sys import stdin
+from math import factorial, sqrt, exp, pi
+
+filename = "demography_data.csv"
+
+def csv_loader():
+    print("Enter in")
+
+def main():
+    arg = []
+    i = 1
+    while (i < len(sys.argv)):
+        arg.append(sys.argv[i])
+        i += 1
+    print(arg)
+
+
+main()
